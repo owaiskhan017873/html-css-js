a798c92653e6dec96164a48bf083c0e3b7f8d533
owaiskhan017873 <okahw.pk@gmail.com>
2025-11-30 22:58:45 +0500

    Add files via upload

---
+++ facebook login foam input field/app.js
@@ -0,0 +1,8 @@
+function login(){
+    let email = document.getElementById("email").value ;
+    let pass = document.getElementById("password").value;
+
+
+    console.log("email :" , email)
+    console.log("password :" , pass)
+}
